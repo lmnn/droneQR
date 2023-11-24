4f6c47af0af428e09e8751db3156191629abe222
tim.malahov <tim.malahov@greenely.se>
2023-11-24 12:34:33 +0200

    feat: Pilot name added, larger qr added, qr moved to the left

--- script.js
+++ script.js
@@ -23,59 +23,82 @@ function showLoader() {
   qrElement.innerHTML = `<img class="w3-spin" src="qr.png" style="height:2rem;opacity:0.5">`;
 }
 
-function generateQRCode(inputName, firmwareType, qrElement) {
+async function generateQRCode(inputName, firmwareType, qrElement) {
   let bgColor = "ffffff00"; // QS Transparent background
+  let bgImageDataColor = 4278190080;
 
   if (firmwareType === "Betaflight") {
     bgColor = "00ff00"; // BF Green background
+    bgImageDataColor = 4278255360;
   }
 
   const canvas = document.createElement("canvas");
   canvas.width = 288;
   canvas.height = 72;
-  const ctx = canvas.getContext("2d");
+  const ctx = canvas.getContext("2d", { alpha: false });
+
+  const generatedQr = new QRious({
+    value: inputName,
+    size: canvas.height,
+  });
+
+  const generatedQrImage = await generatedQr.image;
 
   // Fill the canvas with the background color
   ctx.fillStyle = `#${bgColor}`;
   ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-  const qrUrl = `https://chart.googleapis.com/chart?chs=88x88&cht=qr&chl=${encodeURIComponent(
-    inputName
-  )}`;
+  ctx.textRendering = "geometricPrecision";
+  ctx.font = "24px SF Pixelate Shaded";
+  ctx.fillStyle = "white";
+  ctx.fillText(inputName, 76, 42, 208);
 
-  // Create QR code image
-  const qrImage = new Image();
-  qrImage.crossOrigin = "anonymous";
-  qrImage.src = qrUrl;
+  // Get pixel data from canvas
+  const data32 = new Uint32Array(
+    ctx.getImageData(0, 0, canvas.width, canvas.height).data.buffer
+  );
 
-  qrImage.onload = function () {
-    // Draw QR code on the canvas
-    ctx.drawImage(qrImage, 101, -7); // Position the QR code on the canvas
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  // Refill the canvas with the background color
+  ctx.fillStyle = `#${bgColor}`;
+  ctx.fillRect(0, 0, canvas.width, canvas.height);
 
-    // Create a new image from the canvas data
-    const qrWithBackground = canvas.toDataURL();
+  // Walk through array of canvas pixel data and draw only the ones that aren't the background color
+  for (i = 0; i < data32.length; i++) {
+    if (data32[i] !== bgImageDataColor) {
+      ctx.fillStyle = `#fff`;
+      ctx.fillRect(i % canvas.width, (i / canvas.width) | 0, 1, 1);
 
-    // Create a new image element with the QR code and background
-    const finalQRImage = new Image();
-    finalQRImage.src = qrWithBackground;
-    finalQRImage.style.width = "100%";
-
-    // Display the final QR code image or hide if input is empty
-    qrElement.innerHTML = ""; // Clear previous content
-
-    if (inputName.trim() !== "") {
-      qrElement.appendChild(finalQRImage);
-
-      // Enable download by clicking the QR code image
-      finalQRImage.addEventListener("click", function () {
-        const downloadLink = document.createElement("a");
-        downloadLink.href = qrWithBackground;
-        downloadLink.download =
-          firmwareType + "_" + inputName.trim() + "_qr.png";
-        downloadLink.click();
-      });
-    } else {
-      qrElement.style.display = "none";
+      ctx.fillStyle = `#000`;
+      ctx.fillRect((i % canvas.width) + 1, (i / canvas.width + 1) | 0, 1, 1);
     }
-  };
+  }
+
+  // Draw QR code on the canvas
+  ctx.drawImage(generatedQrImage, 0, 0);
+
+  // Create a new image from the canvas data
+  const qrWithBackground = canvas.toDataURL();
+
+  // Create a new image element with the QR code and background
+  const finalQRImage = new Image();
+  finalQRImage.src = qrWithBackground;
+  finalQRImage.style.width = "100%";
+
+  // Display the final QR code image or hide if input is empty
+  qrElement.innerHTML = ""; // Clear previous content
+
+  if (inputName.trim() !== "") {
+    qrElement.appendChild(finalQRImage);
+
+    // Enable download by clicking the QR code image
+    finalQRImage.addEventListener("click", function () {
+      const downloadLink = document.createElement("a");
+      downloadLink.href = qrWithBackground;
+      downloadLink.download = firmwareType + "_" + inputName.trim() + "_qr.png";
+      downloadLink.click();
+    });
+  } else {
+    qrElement.style.display = "none";
+  }
 }
